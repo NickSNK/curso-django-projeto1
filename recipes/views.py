3d4d79d5bea14583d324cd1cb997415f4e86dbb1
Sergio Godinho <nikdiksnk@gmail.com>
2024-03-31 01:51:28 -0300

    Configured admin page & Added recipe display from database

--- recipes/views.py
+++ recipes/views.py
@@ -2,19 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     context = {
-        'recipes': [make_recipe() for i in range(3)],
+        'recipes': recipes,
     }
-    
     return render(request, 'recipes/pages/home.html', context)
 
-def recipe(request, pk):
+def category(request, id):
+    recipes = Recipe.objects.filter(category__id=id).order_by('-id')
     context = {
-        'recipe': make_recipe(),
+        'recipes': recipes,
+    }
+    return render(request, 'recipes/pages/home.html', context)
+
+def recipe(request, id):
+    recipes = Recipe.objects.get(id=id)
+    context = {
+        'recipe': recipes,
         'is_detail_page': True,
     }
-    
     return render(request, 'recipes/pages/recipe.html', context)
